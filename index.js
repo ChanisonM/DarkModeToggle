979ac0dea91f67ae6b187f6fa6bdd1ace2ae1d06
Chanison Aupathin <89581992+ChanisonM@users.noreply.github.com>
2022-10-17 18:33:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+
+
+
+function updateBody(){
+    if(inputEl.checked){
+        bodyEl.style.background = "black"
+    }else{
+        bodyEl.style.background = "white"
+    }
+}
+
+
+inputEl.addEventListener("input" , ()=>{
+    console.log(inputEl.checked);
+    updateBody();
+
+})
